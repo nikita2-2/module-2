788df1c51ffc3fb93d0ed07f6e27cccecf286f04
Your Namenikita2-2 <nvoskovcev@gmail.com>
2024-08-21 13:21:50 +0300

    3commit

---
+++ module-2/module-2/Header.h
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#define USE_FLOAT 
+#ifdef USE_FLOAT
+typedef float Number;
+#else
+typedef int Number;
+#endif
+
+    Number factorial(Number n) 
+    {
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            return n * factorial(n - 1);
+        }
+    }
+
